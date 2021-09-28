a42f1a91a4290377207666a7760e7aab8850f532
VALISOA <herimalala.val@gmail.com>
2021-09-28 15:36:01 +0200

    login et app user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
